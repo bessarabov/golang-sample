4a3c897c584cbd364a57f17dd5cf0ca79b7ef87d
Ivan Bessarabov <ivan@bessarabov.ru>
2024-03-10 12:24:07 +0000

    0.0.16

--- .github/workflows/create_github_release_description.pl
+++ .github/workflows/create_github_release_description.pl
@@ -4,6 +4,16 @@ use strict;
 use warnings;
 use feature qw(say);
 
+sub write_file {
+    my ($file_name, $content) = @_;
+
+    open(my $fh, '>', $file_name) or die "Could not open file '$file_name' $!";
+
+    print $fh $content;
+    close $fh;
+}
+
+
 sub main {
 
     my $current_tag = $ARGV[0];
@@ -12,7 +22,13 @@ sub main {
         die "Must run as '$0 TAG'"
     }
 
-    say $current_tag;
+    my $release_description = "```
+release_description
+
+$current_tag
+```";
+
+    write_file('release_description.txt', $release_description);
 
 }
 main();
